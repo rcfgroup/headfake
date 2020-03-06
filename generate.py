aa1462b1d7fe6d4b9bbda8e8c399169c0604a970
Rob Free <rcfree@protonmail.com>
2020-03-06 03:44:21 +0000

    Latest changes to uhl_patients.yaml

--- generate.py
+++ generate.py
@@ -5,16 +5,24 @@ conf = config_from_yaml_file("uhl_patients.yaml")
 pprint.pprint(conf)
 fset = conf.get("fieldset")
 for fname, field in fset.fields.items():
-    field.fieldset = fset
+    field.init_from_fieldset(fset)
 
 no_rows = 1000
 output_file = "test.txt"
 with open(output_file, "w") as out:
-    writer = csv.DictWriter(out, list(fset.fields.keys()))
+    fieldnames = []
+    for field in fset.fields.values():
+        fieldnames.extend(field.names)
+    print("fieldnames:" + str(fieldnames))
+    writer = csv.DictWriter(out, fieldnames)
     writer.writeheader()
     for row_no in range(1,no_rows):
         row={}
         for fname, field in fset.fields.items():
-            row[fname] = field.next_value(row)
+            next_val = field.next_value(row)
+            if isinstance(next_val, dict):
+                row.update(next_val)
+            else:
+                row[fname] = next_val
 
         writer.writerow(row)
